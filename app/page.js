2b37b5de0df1f4284e6ae52562e797bd790cad69
Harry Hurst <harryhurst@Harrys-MacBook-Pro.local>
2025-03-10 18:22:33 +0000

    lorem ipsum added to statement

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 // Next.js also provides fonts in the ui component folder
 import { inter } from '@/app/ui/fonts';
 
-import PointerShadow from './components/pointerShadow';
+import PointerShadow from './components/PointerShadow';
 
 // Image component provided by next - stops CLS, does lazy loading etc.
 // import Image from "next/image";
@@ -69,6 +69,33 @@ export default function Home() {
                 within teams or managing tasks independently to deliver
                 high-quality digital experiences to industry standards and web
                 best practices.
+              </p>
+              <p className="mb-4">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum."
+              </p>
+              <p className="mb-4">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum."
+              </p>
+              <p className="mb-4">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum."
               </p>
             </div>
           </main>
