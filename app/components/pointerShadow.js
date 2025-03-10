f9f576e90f9ca9f9cdcac547b8c0b8ab2868f31f
Harry Hurst <harryhurst@Harrys-MacBook-Pro.local>
2025-03-10 16:50:30 +0000

    move component folder into app folder

---
+++ app/components/pointerShadow.js
@@ -0,0 +1,29 @@
+'use client'
+
+import { useState, useEffect } from "react";
+
+const PointerShadow = () => {
+
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      setPosition({ x: e.clientX, y: e.clientY });
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+    return () => window.removeEventListener("mousemove", handleMouseMove);
+  }, []);
+
+  return (
+    <div
+      className="pointer-shadow"
+      style={{
+        left: position.x - 300,
+        top: position.y - 300,
+      }}
+    />
+  )
+}
+
+export default PointerShadow;
